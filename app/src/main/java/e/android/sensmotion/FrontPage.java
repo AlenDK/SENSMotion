a8635b48b9dab263e78dc97532d06d3659022b6f
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-10-25 16:16:36 +0200

    Activity Frontpage

---
+++ app/src/main/java/e/android/sensmotion/FrontPage.java
@@ -0,0 +1,19 @@
+package e.android.sensmotion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FrontPage extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
