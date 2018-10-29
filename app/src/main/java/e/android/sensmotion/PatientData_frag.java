fdd5d9a8fd813e294502f0291958571173d03cec
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2018-10-29 12:06:55 +0100

    Tilføjet patientikon og oprette start på patientdatasiden

---
+++ app/src/main/java/e/android/sensmotion/PatientData_frag.java
@@ -0,0 +1,25 @@
+package e.android.sensmotion;
+
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PatientData_frag extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.patient_data_frag, container, false);
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
