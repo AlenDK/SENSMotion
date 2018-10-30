ab910e4c681c2fec69a62254b998fdaff5ff6d0c
Alen Hasanagic <Alenhasana@yahoo.dk>
2018-10-30 12:35:05 +0100

    Login frag virker, så hvis man logger ind med "admin" kommer man på en anden side, end hvis man bare trykker login.

--- app/src/main/java/e/android/sensmotion/Login_frag.java
+++ app/src/main/java/e/android/sensmotion/Login_frag.java
@@ -8,15 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_frag extends Fragment implements View.OnClickListener {
 
-    String brugernavn;
+    EditText brugernavn;
+    String login;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.login, container, false);
 
+        brugernavn =  view.findViewById(R.id.brugernavn);
 
         Button login = (Button) view.findViewById(R.id.logIndKnap);
 
@@ -29,8 +32,9 @@ public class Login_frag extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
-        brugernavn = view.findViewById(R.id.brugernavn).toString();
-        if (brugernavn == "admin") {
+
+
+        if (brugernavn.getText().toString().matches("admin")) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.fragmentindhold, new NyPatient_frag())
                     .addToBackStack(null)
