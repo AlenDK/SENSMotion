49f6c1c807a0baae361964c0c08231eb56378db3
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2018-10-30 21:04:04 +0100

    prøvet at arbejde lidt med kalenderen og patientdata

--- app/src/main/java/e/android/sensmotion/PatientData_frag.java
+++ app/src/main/java/e/android/sensmotion/PatientData_frag.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.support.v4.app.FragmentTransaction;
 
 public class PatientData_frag extends Fragment implements View.OnClickListener {
 
@@ -14,8 +15,9 @@ public class PatientData_frag extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.patient_data_frag, container, false);
 
-        Button periode = (Button) view.findViewById(R.id.periode);
-        Button graf = (Button) view.findViewById(R.id.grafer);
+        Button periode = view.findViewById(R.id.periode);
+        Button graf = view.findViewById(R.id.grafer);
+
 
         periode.setOnClickListener(this);
         graf.setOnClickListener(this);
@@ -31,7 +33,7 @@ public class PatientData_frag extends Fragment implements View.OnClickListener {
             case R.id.periode:
 
                 getFragmentManager().beginTransaction()
-                        .replace(R.id.Kalenderfragment, new Kalender_frag())
+                        .replace(R.id.kalender, new Kalender_frag())
                         .addToBackStack(null)
                         .commit();
                 break;
