2ddb0031627e3b18dca79cc6dec103d08d2eb2a5
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-11-01 11:54:07 +0100

    Fixed minor flaw concerning the calendarView

--- app/src/main/java/e/android/sensmotion/PatientData_frag.java
+++ app/src/main/java/e/android/sensmotion/PatientData_frag.java
@@ -11,10 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.CalendarView;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class PatientData_frag extends Fragment implements View.OnClickListener {
 
+    boolean calendar = false;
+    CalendarView calendarView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,9 +28,11 @@ public class PatientData_frag extends Fragment implements View.OnClickListener {
 
         Button periode = (Button) view.findViewById(R.id.periode);
         Button graf = (Button) view.findViewById(R.id.grafer);
+        calendarView = (CalendarView) view.findViewById(R.id.calendarView);
 
         periode.setOnClickListener(this);
         graf.setOnClickListener(this);
+        calendarView.setVisibility(View.GONE);
 
         return view;
     }
@@ -51,10 +58,14 @@ public class PatientData_frag extends Fragment implements View.OnClickListener {
         switch (view.getId()) {
             case R.id.periode:
 
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.kalender, new Kalender_frag())
-                        .addToBackStack(null)
-                        .commit();
+                if(!calendar) {
+                    calendarView.setVisibility(View.VISIBLE);
+                    calendar = true;
+                } else if(calendar) {
+                    calendarView.setVisibility(View.GONE);
+                    calendar = false;
+                }
+
                 break;
 
 
