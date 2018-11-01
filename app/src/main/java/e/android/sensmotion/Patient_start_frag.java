36f60741d50e09a51d22021e300ccdc6572c4af8
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-11-01 12:52:57 +0100

    Changed Patient_start from activity to fragment

--- app/src/main/java/e/android/sensmotion/Patient_start_frag.java
+++ app/src/main/java/e/android/sensmotion/Patient_start_frag.java
@@ -1,46 +1,49 @@
 package e.android.sensmotion;
 
 
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Patient_start_frag extends AppCompatActivity implements View.OnClickListener{
+public class Patient_start_frag extends Fragment implements View.OnClickListener{
 
     private ImageView imageView, stickman_walk, stickman_stand, stickman_bike, stickman_train, stickman_other;
     private ImageButton profile_button;
     private TextView textView;
     private ProgressBar walk,stand,bike,train,other;
     private int walk_prog = 0;
+    Toast toast;
 
     private Handler progHandle = new Handler();
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_patient);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_patientliste_frag, container, false);
 
-        imageView = findViewById(R.id.actionbar_image);
-        stickman_walk = findViewById(R.id.walking_stickman);
-        stickman_stand = findViewById(R.id.standing_stickman);
-        stickman_bike = findViewById(R.id.biking_stickman);
-        stickman_train = findViewById(R.id.training_stickman);
-        stickman_other = findViewById(R.id.other_stickman);
+        imageView = view.findViewById(R.id.actionbar_image);
+        stickman_walk = view.findViewById(R.id.walking_stickman);
+        stickman_stand = view.findViewById(R.id.standing_stickman);
+        stickman_bike = view.findViewById(R.id.biking_stickman);
+        stickman_train = view.findViewById(R.id.training_stickman);
+        stickman_other = view.findViewById(R.id.other_stickman);
 
-        profile_button = findViewById(R.id.knap_profil);
+        profile_button = view.findViewById(R.id.knap_profil);
 
-        textView = findViewById(R.id.nameText);
+        textView = view.findViewById(R.id.nameText);
 
-        walk = findViewById(R.id.progbar_walk);
-        stand = findViewById(R.id.progbar_stand);
-        bike = findViewById(R.id.progbar_bike);
-        train = findViewById(R.id.progbar_train);
-        other = findViewById(R.id.progbar_other);
+        walk = view.findViewById(R.id.progbar_walk);
+        stand = view.findViewById(R.id.progbar_stand);
+        bike = view.findViewById(R.id.progbar_bike);
+        train = view.findViewById(R.id.progbar_train);
+        other = view.findViewById(R.id.progbar_other);
 
 
         new Thread(new Runnable() {
@@ -58,11 +61,12 @@ public class Patient_start_frag extends AppCompatActivity implements View.OnClic
                 }
 
                 if(walk_prog == 100){
-                    Popup popup = new Popup();
-                    popup.show(getSupportFragmentManager(), "task complete!");
+                    Toast.makeText(getActivity(), "Godt klaret. Du har nået en af dine" +
+                            "daglige mål for i dag!", Toast.LENGTH_LONG).show();
                 }}
         }).start();
 
+        return view;
     }
 
     @Override
