696a2f1e850c0614b9f9b46bb31214e73241df89
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-11-23 21:15:37 +0100

    Connection with an online MySQL database

--- app/src/main/java/e/android/sensmotion/connector/Connector.java
+++ app/src/main/java/e/android/sensmotion/connector/Connector.java
@@ -4,6 +4,15 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import e.android.sensmotion.JDBC_Interfaces.DALException;
+
+
 public class Connector {
 
     public void connectURL(String urlInput)  throws IOException {
@@ -13,5 +22,122 @@ public class Connector {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.setRequestProperty("", "");
+    }
+
+    /**
+     * To connect to a MySQL-server
+     *
+     * @param url
+     *            must have the form "jdbc:mysql://<server>/<database>" for default
+     *            port (3306) OR "jdbc:mysql://<server>:<port>/<database>" for
+     *            specific port more formally "jdbc:subprotocol:subname"
+     *
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws SQLException
+     */
+    public static Connection connectToDatabase(String url, String username, String password)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        // call the driver class' no argument constructor
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+        // get Connection-object via DriverManager
+        return (Connection) DriverManager.getConnection(url, username, password);
+    }
+
+    private static Connection conn;
+    private static Statement stm;
+    private static Connector connector;
+
+    /**
+     * The connector now becomes a singleton, so several connections aren't established.
+     * Only one is at first instantiated and since passed around.
+     * @return Returns the connector instance.
+     * @throws DALException
+     */
+    public static Connector getInstance() throws DALException {
+        if (connector==null || stm ==null || conn == null) {
+            try {
+                connector = new Connector();
+                Thread ping = new Thread(new Pingtool());
+                ping.start();
+            } catch (InstantiationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                throw new DALException(e.getMessage());
+            } catch (IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                throw new DALException(e.getMessage());
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                throw new DALException(e.getMessage());
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                throw new DALException(e.getMessage());
+            }
+        }
+        return connector;
+    }
+
+    /**
+     * Constructor for establishing connection right away.
+     * @param server
+     * @param port
+     * @param database
+     * @param username
+     * @param password
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connector(String server, int port, String database, String username, String password)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        conn = connectToDatabase("jdbc:mysql://" + server + ":" + port + "/" + database, username, password);
+        stm = conn.createStatement();
+    }
+
+    /**
+     * Constructor without parameters.
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public Connector() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        this(Constant.server, Constant.port, Constant.database, Constant.username, Constant.password);
+    }
+
+    /**
+     * Performs a query on the database and returns a resultset object.
+     * @param cmd The statement we wish to execute.
+     * @return The resultset, result of the query.
+     * @throws DALException
+     */
+    public ResultSet doQuery(String cmd) throws DALException {
+        try {
+            stm = conn.createStatement();
+            return stm.executeQuery(cmd);
+        } catch (SQLException e) {
+            throw new DALException(e);
+        }
+    }
+
+    /**
+     * Performs an update. Returns an integer that tells how well the process went.
+     * @param cmd The statement we wish to execute.
+     * @return
+     * @throws DALException
+     */
+    public int doUpdate(String cmd) throws DALException {
+        try {
+            return stm.executeUpdate(cmd);
+        } catch (SQLException e) {
+            throw new DALException(e);
+        }
     }
 }
---
+++ app/src/main/java/e/android/sensmotion/connector/Constant.java
@@ -0,0 +1,20 @@
+package e.android.sensmotion.connector;
+
+
+/**
+ * Contains information about database - how to connect.
+ * Connector makes use of this information.
+ * @author Group 11
+ *
+ */
+public abstract class Constant
+{
+	public static final String
+	server					= "us-cdbr-gcp-east-01.cleardb.net",  // database-serveren
+	database				= "gcp_b62c0105038a8eabee03",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+	username				= "bab5f0b0b49367", // dit brugernavn = dit studienummer
+	password				= "80baff82"; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 3306;
+}
