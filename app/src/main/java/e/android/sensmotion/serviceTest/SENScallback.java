fef90520962221945974ebb8f306c6568fc340bc
Unknown <s175195@student.dtu.dk>
2018-11-25 14:57:58 +0100

    API work work work??
    Maybe

--- app/src/main/java/e/android/sensmotion/serviceTest/SENScallback.java
+++ app/src/main/java/e/android/sensmotion/serviceTest/SENScallback.java
@@ -1,8 +1,8 @@
 package e.android.sensmotion.serviceTest;
 
-import e.android.sensmotion.dataTest.Value;
+import e.android.sensmotion.dataTest.Values;
 
 public interface SENScallback {
-    void serviceSuccess(Value value);
+    void serviceSuccess(Values values);
     void serviceFailure(Exception exception);
 }
--- app/src/main/java/e/android/sensmotion/serviceTest/SENSservice.java
+++ app/src/main/java/e/android/sensmotion/serviceTest/SENSservice.java
@@ -3,6 +3,9 @@ package e.android.sensmotion.serviceTest;
 import android.net.Uri;
 import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +14,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLConnection;
+
+import e.android.sensmotion.dataTest.JSONPopulator;
+import e.android.sensmotion.dataTest.Values;
 
 public class SENSservice {
     private SENScallback callback;
@@ -47,18 +53,51 @@ public class SENSservice {
                     while((line = reader.readLine()) != null){
                         restult.append(line);
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
 
+                    return restult.toString();
+
+                } catch (Exception e) {
+                    error.printStackTrace();
+                }
                 return null;
             }
 
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
+
+                if(s == null && error != null){
+                    callback.serviceFailure(error);
+                    return;
+                }
+
+                try{
+                    JSONObject data = new JSONObject(s);
+                    JSONObject queryResults = data.optJSONObject("query");
+
+                    int count = queryResults.optInt("count");
+                    if(count == 0){
+                        callback.serviceFailure(new LocalPatientException("Ingen patienter fundet for følgende nøgle: " + patient_key));
+                        return;
+                    }
+
+                    Values values = new Values();
+                    values.populate(queryResults.optJSONObject("value").optJSONObject("values"));
+
+                    callback.serviceSuccess(values);
+
+                } catch (JSONException e) {
+                    callback.serviceFailure(e);
+                }
             }
 
         }.execute(patient_key);
+    }
+
+    //Lokal error
+    public class LocalPatientException extends Exception{
+        public LocalPatientException(String detailMessage) {
+            super(detailMessage);
+        }
     }
 }
