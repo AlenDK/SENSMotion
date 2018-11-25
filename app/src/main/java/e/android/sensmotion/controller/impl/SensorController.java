9ea3311b8a8c76ac7649d77c70e3fd979f92812f
Tobias Hagemann <32563473+TBHagemann@users.noreply.github.com>
2018-11-25 14:51:00 +0100

    Oprettet controllerRegistry og controllere

---
+++ app/src/main/java/e/android/sensmotion/controller/impl/SensorController.java
@@ -0,0 +1,6 @@
+package e.android.sensmotion.controller.impl;
+
+import e.android.sensmotion.controller.interfaces.ISensorController;
+
+public class SensorController implements ISensorController {
+}
