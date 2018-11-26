cbde14be978fd3eaa6e36ff991d46b8b74d61c37
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-11-26 20:40:44 +0100

    I CRY

--- app/src/main/java/e/android/sensmotion/controller/interfaces/IDataController.java
+++ app/src/main/java/e/android/sensmotion/controller/interfaces/IDataController.java
@@ -1,4 +1,8 @@
 package e.android.sensmotion.controller.interfaces;
 
+import e.android.sensmotion.dataTest.Value;
+
 public interface IDataController {
+    void serviceSuccess(Value value);
+    void serviceFailure(Exception exception);
 }
