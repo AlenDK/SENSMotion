f38c872dc23a399133963fe41c04d8210e7a295a
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2018-11-26 14:39:41 +0100

    I cry

--- app/src/main/java/e/android/sensmotion/JDBC_Interfaces/DALException.java
+++
@@ -1,8 +0,0 @@
-package e.android.sensmotion.JDBC_Interfaces;
-
-public class DALException extends Exception
-{
-	private static final long serialVersionUID = -5490114628089339322L;
-	public DALException(String message) { super(message); }    
-	public DALException(Exception e) { super(e); }
-}
--- app/src/main/java/e/android/sensmotion/JDBC_Interfaces/SensorDAO.java
+++
@@ -1,4 +0,0 @@
-package e.android.sensmotion.JDBC_Interfaces;
-
-public interface SensorDAO {
-}
