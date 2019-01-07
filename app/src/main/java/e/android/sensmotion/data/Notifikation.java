a69f6072ac87adfb80c6accde9563d9d54c5d01b
Johannes Nissen <nisse@LAPTOP-HAU53GTA.home>
2019-01-07 23:39:06 +0100

    Tilføjede Klasse til oprettelse af "NotificationChannels"

---
+++ app/src/main/java/e/android/sensmotion/data/Notifikation.java
@@ -0,0 +1,41 @@
+package e.android.sensmotion.data;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class Notifikation extends Application {
+    final static String CHANNEL_ID1 = "Channel1";
+    final static String CHANNEL_ID2 = "Channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationchannels();
+    }
+
+    public void createNotificationchannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_ID1,
+                    "channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is channel 1");
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_ID2,
+                    "channel 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("This is channel 2");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+
+        }
+    }
+
+}
