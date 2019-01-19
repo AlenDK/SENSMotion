dc04868579abde044f32f180b9a87f0add0ae864
Unknown <s175446@student.dtu.dk>
2019-01-19 23:19:51 +0100

    Lena plug

--- app/src/main/java/e/android/sensmotion/views/RecyclerViewAdapter.java
+++ app/src/main/java/e/android/sensmotion/views/RecyclerViewAdapter.java
@@ -1,7 +1,10 @@
 package e.android.sensmotion.views;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,21 +29,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     ViewHolder holder;
     LinearLayout linearLayout;
     ConstraintLayout constraintLayout;
+    private onClickRecycle cr;
 
-    int positionHolder = 0;
 
-
-    public RecyclerViewAdapter(Context context, RecyclerView recyclerView, ArrayList<String> days, ArrayList<Integer> images) {
+    public RecyclerViewAdapter(Context context, RecyclerView recyclerView, ArrayList<String> days, ArrayList<Integer> images, onClickRecycle cr) {
         this.context = context;
         this.recyclerView = recyclerView;
         this.days = days;
         this.images = images;
+        this.cr = cr;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listview_list_item, parent, false);
-        holder = new ViewHolder(view);
+        holder = new ViewHolder(view, cr);
         linearLayout = view.findViewById(R.id.linearLayout);
         constraintLayout = view.findViewById(R.id.constraintLayout);
         recyclerView = view.findViewById(R.id.previousList);
@@ -54,12 +57,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.iv.setImageResource(images.get(position));
 
 
-        holder.cl.setOnClickListener(new View.OnClickListener() {
+      /*  holder.cl.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                holder.cl.setBackgroundResource(R.color.colorGreen);
+
             }
-        });
+        });*/
     }
 
     @Override
@@ -73,16 +77,28 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return position;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder  {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
         public TextView tv;
         public ImageView iv;
         public ConstraintLayout cl;
+        public onClickRecycle cr;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, onClickRecycle cr) {
             super(itemView);
+            this.cr = cr;
             tv = itemView.findViewById(R.id.previousTitle);
             iv = itemView.findViewById(R.id.previousSmiley);
             cl = itemView.findViewById(R.id.constraintLayout);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            cr.clickItem(getAdapterPosition());
+        }
+    }
+
+    public interface onClickRecycle{
+        void clickItem(int position);
     }
 }
